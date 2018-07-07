be3b39e6a94ec0db2d75fb9772016158bed656c7
Nick Suwyn <nicholas_suwyn@suwynitsolutions.com>
2018-07-07 14:41:27 -0700

    week 4

---
+++ week_4/src/assignments/Coding.java
@@ -0,0 +1,19 @@
+package assignments;
+
+public class Coding {
+	
+	/*
+	 * Create a new menu driven application using a List instead of an array. 
+	 * The application should allow the user to manage a friends list.
+	 * The application should have the following options:
+	 * 1) Display All Friends
+	 * 2) Add New Friend
+	 * 3) Delete All Friends
+	 * 4) Delete A Friend
+	 * 
+	 * In a different class, create an ArrayList, HashSet, and HashMap.
+	 * Add at least three entries to each collection.
+	 * Iterate over each collection and print out the contents.
+	 */
+
+}
---
+++ week_4/src/assignments/Research.java
@@ -0,0 +1,18 @@
+package assignments;
+
+public class Research {
+	
+	/*
+	 * Research the differences between Lists, Sets, and Maps in Java.
+	 * Find different types of each (List, Set, Map) and research when
+	 * you would use each implementation, or when a specific implementation is
+	 * better than another. Document your findings.
+	 * 
+	 * In addition, include a line of code that shows how to instantiate each of 
+	 * the following:
+	 * ArrayList of type String
+	 * HashSet of type StringBuilder
+	 * HashMap of type String, String
+	 */
+
+}
